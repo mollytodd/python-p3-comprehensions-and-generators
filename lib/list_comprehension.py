18b13efca37220925387952ae68c13d285b6109d
Molly Todd <mollyjt89@gmail.com>
2023-09-19 15:38:51 -0700

    completed lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers = []
+    for number in num_list:
+        if number % 2 == 0:
+            even_numbers.append(number)
+    return even_numbers
+
 
 def make_exclamation(sentence_list):
-    pass
+    exclimation_list = [word +"!" for word in sentence_list]
+    return exclimation_list
